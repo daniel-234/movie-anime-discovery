f914b61829b578465c6aeff8da1566e882c6f2cc
Daniele <danielerb.dev@gmail.com>
2026-03-31 18:02:58 +0200

    Feat: Fetch data from the TMDB endpoint

---
+++ main.py
@@ -0,0 +1,35 @@
+from decouple import config
+import httpx
+
+TMDB_URL = "https://api.themoviedb.org/3"
+
+TMDB_TOKEN = config("TMDB_TOKEN")
+
+headers = {"Authorization": f"Bearer {TMDB_TOKEN}"}
+
+def _fetch_tmdb_data(endpoint: str) -> dict:
+    """
+    Retrieve information from a TMDB API endpoint
+    """
+    client = httpx.Client(base_url=TMDB_URL, headers=headers)
+    try:
+        response = client.get(endpoint)
+        response.raise_for_status()
+        return response.json()
+    except httpx.HTTPStatusError as e:
+        print(
+            f"Failed to fetch data for {endpoint} for season {year}: {e.response.status_code}"
+        )
+        return None
+    except httpx.RequestError as e:
+        print(f"Network error: {e}")
+        return None
+
+
+def main():
+    data = _fetch_tmdb_data("/trending/movie/week")
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
