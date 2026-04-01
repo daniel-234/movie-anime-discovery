2a157e0eac417e6249963f43438ccc1eca87f663
Daniele <danielerb.dev@gmail.com>
2026-04-01 09:13:06 +0200

    Fix: Correct bugs as per review

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ TMDB_URL = "https://api.themoviedb.org/3"
 
 TMDB_TOKEN = config("TMDB_TOKEN")
 
-headers = {"Authorization": f"Bearer {TMDB_TOKEN}"}
+HEADERS = {"Authorization": f"Bearer {TMDB_TOKEN}"}
 
-def _fetch_tmdb_data(endpoint: str) -> dict:
+def _fetch_tmdb_data(endpoint: str) -> dict | None:
     """
     Retrieve information from a TMDB API endpoint
     """
-    client = httpx.Client(base_url=TMDB_URL, headers=headers)
+    client = httpx.Client(base_url=TMDB_URL, headers=HEADERS)
     try:
         response = client.get(endpoint)
         response.raise_for_status()
         return response.json()
     except httpx.HTTPStatusError as e:
         print(
-            f"Failed to fetch data for {endpoint} for season {year}: {e.response.status_code}"
+            f"Failed to fetch data for {endpoint}: {e.response.status_code}"
         )
         return None
     except httpx.RequestError as e:
