4576914a9aa8764b3e11f6be9e3883981e6cbfe8
Daniele <danielerb.dev@gmail.com>
2026-04-04 20:00:15 +0200

    Create a movie app

--- media/models.py
+++ media/models.py
@@ -1,1 +1,19 @@
-# Create your models here.
+from django.db import models
+
+
+class Movie(models.Model):
+    adult = models.BooleanField
+    backdrop_path = models.CharField(max_length=50)
+    id = models.IntegerField
+    title = models.CharField(max_length=50)
+    original_title = models.CharField(max_length=50)
+    overview = models.CharField(max_length=250)
+    poster_path = models.CharField(max_length=250)
+    media_type = models.CharField(max_length=20)
+    original_language = models.CharField(max_length=20)
+    genre_ids = models.JSONField(default=list)
+    popularity = models.IntegerField
+    release_date = models.CharField(max_length=250)
+    video = models.BooleanField
+    vote_average = models.IntegerField
+    vote_count = models.IntegerField
--- media/views.py
+++ media/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render
+
+from .tmdb import get_movie_list_from_api
+
+
+def movie_list(request):
+    movies = get_movie_list_from_api("/trending/movie/week")
+    return render(request, "media/movie/list.html", {"movies": movies})
