4c1bc91b57078f0e3d70b5d7f84194508e931ab1
Daniele <danielerb.dev@gmail.com>
2026-04-14 22:51:04 +0200

    Refactor: Render both lists in homepage

--- media/views.py
+++ media/views.py
@@ -3,19 +3,18 @@ from django.shortcuts import get_object_or_404, render
 from .models import Anime, Movie
 
 
-def movie_list(request):
-    movies = Movie.objects.all()[:5]
-    return render(request, "media/movie/list.html", {"movies": movies})
+def home(request):
+    context = {
+        "movie_list": Movie.objects.all()[:5],
+        "anime_list": Anime.objects.all()[:5],
+    }
+
+    return render(request, "media/home.html", context)
 
 
 def movie_detail(request, id):
     movie = get_object_or_404(Movie, id=id)
     return render(request, "media/movie/detail.html", {"movie": movie})
-
-
-def anime_list(request):
-    anime = Anime.objects.all()[:5]
-    return render(request, "media/anime/list.html", {"anime_list": anime})
 
 
 def anime_detail(request, id):
