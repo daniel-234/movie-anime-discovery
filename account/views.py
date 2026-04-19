966eb45072f02bd3ae0c98f0ce366cb23979994d
Daniele <danielerb.dev@gmail.com>
2026-04-19 18:57:19 +0200

    Feat: Add a signup page

--- account/views.py
+++ account/views.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 @login_required
 def dashboard(request):
     return render(request, "account/dashboard.html", {"section": "dashboard"})
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
