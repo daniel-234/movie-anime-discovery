fee7562ada70d3246e297e107048c9feacaa9a62
Daniele <danielerb.dev@gmail.com>
2026-04-19 23:31:47 +0200

    Feat: Extend Django User - Reference #24

--- account/urls.py
+++ account/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("edit/", views.edit_profile, name="edit_profile"),
     path("", views.dashboard, name="dashboard"),
 ]
