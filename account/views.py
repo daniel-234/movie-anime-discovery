a475189df11641e560981350e5687ae21d9c6023
Daniele <danielerb.dev@gmail.com>
2026-04-29 11:57:25 +0200

    Refactor: Encapsulate update logic in a model method - Reference #27

--- account/views.py
+++ account/views.py
@@ -1,7 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.db import transaction
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.decorators.cache import never_cache
@@ -24,14 +23,14 @@ class SignUpView(CreateView):
 
 @never_cache
 @login_required
-@transaction.atomic
 def edit_profile(request):
     if request.method == "POST":
         user_form = UserEditForm(request.POST, instance=request.user)
         profile_form = ProfileEditForm(request.POST, instance=request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
+            request.user.profile.update(
+                user_form.cleaned_data, profile_form.cleaned_data
+            )
             messages.success(request, "Your profile was successfully updated.")
             return redirect("dashboard")
     else:
