a221329abbbaeff55a1f5b65f942e1602d70bbaf
Bob Belderbos <bobbelderbos@gmail.com>
2026-04-30 16:37:11 +0200

    update tests to test less core django, more own code

--- tests/test_account.py
+++ tests/test_account.py
@@ -9,21 +9,26 @@ class ProfileSignalTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user("alice", password="pass1234!")
 
-    def test_profile_exists_for_user(self):
-        assert self.user.username == "alice"
+    def test_user_object_creation_also_creates_related_profile(self):
         self.assertTrue(Profile.objects.filter(user=self.user).exists())
 
-    def test_profile_bio(self):
-        self.user.profile.bio = "Movie lover"
-        self.user.save()
-
-        self.user.refresh_from_db()
-        assert self.user.profile.bio != "Movie lover"
-
-    def test_profile_delete(self):
+    def test_deleting_user_also_deletes_related_profile_entry(self):
         self.user.delete()
-
         self.assertTrue(Profile.objects.count() == 0)
+
+    def test_deleting_profile_user_remains(self):
+        # cascade delete means you delete the related model
+        # but we learned that the user is the parent and the profile
+        # is the child, so deleting the profile should not delete the user
+        self.user.profile.delete()
+        self.assertTrue(User.objects.count() == 1)
+
+    def test_update_profile(self):
+        self.user.profile.update(
+            user_data={"username": "bob"}, profile_data={"bio": "Movie lover"}
+        )
+        assert self.user.username == "bob"
+        assert self.user.profile.bio == "Movie lover"
 
 
 class SignupViewTest(TestCase):
